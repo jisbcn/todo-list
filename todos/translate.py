61b40a5cde52a1fa6227a0e63276e8bcdc0766db
jisbcn <joseisantoyo@gmail.com>
2021-05-14 17:08:51 +0000

    translate bueno

--- todos/translate.py
+++ todos/translate.py
@@ -11,21 +11,21 @@ def translate(event, context):
    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
             
 # fetch todo from the database
-    result = table.get_item(
+   result = table.get_item(
           Key={
               'id': event['pathParameters']['id']
               }
                             )
-    lenguaje = event['pathParameters']['language']
+   lenguaje = event['pathParameters']['language']
 
-    client = boto3.client('translate', region_name="us-east-1")
-    traduce=client.translate_text(Text=result['Item']['text'], 
+   client = boto3.client('translate', region_name="us-east-1")
+   traduce=client.translate_text(Text=result['Item']['text'], 
             SourceLanguageCode="auto", TargetLanguageCode=lenguaje)
     
-    result['Item']['text']=traduce['TranslatedText']
-    response = {
+   result['Item']['text']=traduce['TranslatedText']
+   response = {
             "statusCode": 200,
             "body": json.dumps(result['Item'],
-            cls=decimalencoder.DecimalEncoder)
-            }                                                        }
-return response
+                         cls=decimalencoder.DecimalEncoder)
+            }                                                       
+   return response
